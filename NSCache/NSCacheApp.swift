247095d9b9d364a44451e15df414e881b57fccba
Batuhan <kucukyildizbatuhan@gmail.com>
2023-09-10 01:05:41 +0300

    Initial Commit

---
+++ NSCache/NSCacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSCacheApp.swift
+//  NSCache
+//
+//  Created by Batuhan Küçükyıldız on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct NSCacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
